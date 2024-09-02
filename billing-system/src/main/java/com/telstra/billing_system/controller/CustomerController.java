33d4d2c4684b82714ff15516833d4dfc5fa09dda
NandanVasistaBH <nandan.vasista.bh@gmail.com>
2024-09-02 09:44:38 +0530

    check for presense for the fields needed to register and login

--- billing-system/src/main/java/com/telstra/billing_system/controller/CustomerController.java
+++ billing-system/src/main/java/com/telstra/billing_system/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.telstra.billing_system.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +13,18 @@ public class CustomerController {
     @Autowired
     private CustomerService service;
     @PostMapping("/customer-register")
-    public String register(@RequestBody Customer customer){
-        System.out.println(customer);
-        return service.register(customer);
+    public ResponseEntity<String>  register(@RequestBody Customer customer){
+        if(customer==null || customer.getName()==null || customer.getCustPassword()==null || customer.getCustEmail()==null || customer.getCustPhoneNo()==null){
+            return new ResponseEntity<>("need to provide both name,password,phone and email",HttpStatus.BAD_GATEWAY);
+        }
+        return new ResponseEntity<>( service.register(customer),HttpStatus.OK);
     }
     @PostMapping("/customer-login")
-    public String login(@RequestBody Customer customer){
-        System.out.println(customer);
-        return service.verify(customer);
+    public ResponseEntity<String> login(@RequestBody Customer customer){
+        if(customer==null || customer.getName()==null || customer.getCustPassword()==null){
+            return new ResponseEntity<>("need to provide both name and password",HttpStatus.BAD_GATEWAY);
+        }
+        return new ResponseEntity<>(service.verify(customer),HttpStatus.OK);
     }
     @GetMapping("/hello-world")
     public String helloWorld(){
