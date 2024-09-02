e687cb584f12e53480f1979158135f4f9088c018
NandanVasistaBH <nandan.vasista.bh@gmail.com>
2024-09-02 09:30:02 +0530

    removed unused imports

--- billing-system/src/main/java/com/telstra/billing_system/filters/JwtFilter.java
+++ billing-system/src/main/java/com/telstra/billing_system/filters/JwtFilter.java
@@ -1,7 +1,6 @@
 package com.telstra.billing_system.filters;
 
 import java.io.IOException;
-import org.springframework.context.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
