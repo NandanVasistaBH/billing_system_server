57131424d831fa4d792c835350c4e2a2d4d7ce66
NandanVasistaBH <nandan.vasista.bh@gmail.com>
2024-09-04 11:33:33 +0530

    increased jwt token validity time to 24hrs

--- billing-system/src/main/java/com/telstra/billing_system/service/JwtService.java
+++ billing-system/src/main/java/com/telstra/billing_system/service/JwtService.java
@@ -23,7 +23,7 @@ public class JwtService {
                     .add(claims)
                     .subject(username)
                     .issuedAt(new Date(System.currentTimeMillis()))
-                    .expiration(new Date(System.currentTimeMillis()+60*60*60)) // 1hr
+                    .expiration(new Date(System.currentTimeMillis()+24 * 60 * 60 * 1000)) // 24 hours
                     .and()
                     .signWith(getKey())
                     .compact();
