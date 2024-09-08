4a6a693213508aaa3a8f1c4fc6a0a824a6d75793
NandanVasistaBH <nandan.vasista.bh@gmail.com>
2024-09-08 18:54:59 +0530

    hiding jwt secret key

--- billing-system/src/main/java/com/telstra/billing_system/service/JwtService.java
+++ billing-system/src/main/java/com/telstra/billing_system/service/JwtService.java
@@ -1,6 +1,7 @@
 package com.telstra.billing_system.service;
 
 import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import java.util.Date;
@@ -14,7 +15,8 @@ import java.util.HashMap;
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY = "vTQ6Usc4uKsmOhd2ZYQ9vD/qg6QjpK5y4/g2ZrW80EY=";
+    @Value("${jwt.secret.key}")
+    private String SECRET_KEY;
     
     public String generateToken(String username){
         Map<String,Object> claims= new HashMap<>();
