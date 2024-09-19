469b814be6dacc2284e91764a765f86acee6a750
NandanVasistaBH <nandan.vasista.bh@gmail.com>
2024-09-19 13:56:32 +0530

    Horizontal Pod Autoscaler (HPA) wasn't working correctly earlier as deployment/metrics-server wasn't running correctly

--- billing-system/src/main/java/com/telstra/billing_system/utils/ApiLoadTest.java
+++ billing-system/src/main/java/com/telstra/billing_system/utils/ApiLoadTest.java
@@ -1,4 +1,5 @@
 package com.telstra.billing_system.utils;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -6,13 +7,15 @@ import java.net.http.HttpResponse;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
 // the spring dev server is running on 10000 on dockerizing we will expose 10000 from container
 // again this 10000 port is exposed to the POD which also accepts request @10000
 // but pod lives in a k8 cluster so access this pod in the cluster use NODE PORT 30001
 public class ApiLoadTest {
     private static final String URL = "http://localhost:30001/subscriptions/prepaid";
-    private static final int NUM_THREADS = 10; // Number of concurrent threads
-    private static final int REQUESTS_PER_THREAD = 10000;
+    private static final int NUM_THREADS = 50;
+    private static final int REQUESTS_PER_THREAD = 20000;
+
     public static void main(String[] args) {
         HttpClient client = HttpClient.newHttpClient();
         ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
