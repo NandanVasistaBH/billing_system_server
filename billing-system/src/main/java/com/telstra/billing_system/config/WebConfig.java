99a90d30d22c4d1d1f6d59efa4434cf6bad5f5db
NandanVasistaBH <nandan.vasista.bh@gmail.com>
2024-09-25 11:57:12 +0530

    removing * to correct uri

--- billing-system/src/main/java/com/telstra/billing_system/config/WebConfig.java
+++ billing-system/src/main/java/com/telstra/billing_system/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")  
+                .allowedOrigins(frontendOrigin)  
                 .allowedMethods("*")                    
                 .allowedHeaders("*") 
                 .allowCredentials(true);
